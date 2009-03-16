6ce52c68aebd108279075eb8dced0d7f2167f2ba
david.kadish <david.kadish@d0deb880-f30d-11dd-99c0-75d572ba1ddd>
2009-03-16 06:17:26 +0000

    plot changes

--- trunk/Lab2/code/lab2p3.m
+++ trunk/Lab2/code/lab2p3.m
@@ -1,8 +1,10 @@
 load('c:\Documents and Settings\dkadish\workspace\lab2\code\lab2_3.mat')
 se = SequentialEstimation({a b});
 
-p_err = cell(20);
-for i = 1:20
-    conf = se.ConfusionMatrix(i);
-    p_err{i} = (conf(1,2) + conf(2,1))*100/sum(sum(conf));
+p_err = zeros(5,20);
+for i = 1:5
+    for j = 1:20
+        conf = se.ConfusionMatrix(i);
+        p_err(i,j) = (conf(1,2) + conf(2,1))*100/sum(sum(conf));
+    end
 end
